27a45c49cc91ea671f863a9f90ac34a2c8c5b265
swapnilaryan97 <saiamthere@gmail.com>
2022-02-28 22:17:04 -0500

    added about_test file

--- tests/homepage_test.py
+++ tests/homepage_test.py
@@ -5,8 +5,3 @@ def test_request_example(client):
     """This makes the index page"""
     response = client.get("/")
     assert b"Lorem ipsum" in response.data
-
-
-def test_request_about(client):
-    response = client.get('/about')
-    assert b"about" in response.data
